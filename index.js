359c9b1d0d82086996edcfc2387f7946d9d78ed7
chuchuyun <happydays0509@gmail.com>
2016-07-10 20:07:25 +0800

    console.log star width*height

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-function ex01 () {
+function generateStar () {
     console.log("Hello, World!");
+    var width = document.getElementById("width").value
+    var height = document.getElementById("height").value
+    
+    width = parseInt(width, 10);
+    height = parseInt(height, 10);
+    console.log(width,height);
+    var result = "";
+    for (var i=0; i<height; i++) {
+    	for (var j=0; j<width; j++) {
+    		result += '*';
+    	}
+    	result += '\n';	
+    }
+    console.log(result)
 }
-ex01();
 
